eb7c01015ab98a64ee12a98f386c95e3ff60db92
Prasadbot778 <111696957+Prasadbot778@users.noreply.github.com>
2022-08-22 01:09:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94760684787']
+global.premium = ['+94760684787']
+global.ownernomer = '+94760684787'
+global.ownername = '🐦prasad'
+global.botname = 'prasad'
 global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.ig = 'https://youtube.com/channel/UCnZDAzT0m6ELHdTitdnpFqw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://youtube.com/channel/UCnZDAzT0m6ELHdTitdnpFqw'
+global.packname = 'prasad'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
