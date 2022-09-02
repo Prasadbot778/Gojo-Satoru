d4414205b94f01c7919bb3c9e750750144ed5f9d
Prasadbot778 <111696957+Prasadbot778@users.noreply.github.com>
2022-09-03 00:19:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94760684787']
+global.premium = ['94760684787']
+global.ownernomer = '94760684787'
+global.ownername = '🐦prasad king'
+global.botname = 'prasad'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCnZDAzT0m6ELHdTitdnpFqw'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
